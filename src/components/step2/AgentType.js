14963c14b7117907de1985c8371a0293db8fc90b
engradilhameed <adilhameed200@gmail.com>
2023-05-30 01:37:52 +0500

    bugfix

--- src/components/step2/AgentType.js
+++ src/components/step2/AgentType.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function AgentType() {
+    //      const handleChange = (event) => {
+    //     const selectedOption = event.target.value;
+    //     if (selectedOption !== '') {
+    //       const selectedText = event.target.options[event.target.selectedIndex].text;
+    //       alert('Selected option: ' + selectedText);
+    //     }
+    //   };
     return (
         <div className='container-fluid'>
 
@@ -10,13 +17,15 @@ export default function AgentType() {
                 <div className="row w-80 mx-auto text-white">
                     <div className="text-center mx-auto col-md-6 px-md-4 px-0 formgray-text">
                         <label htmlFor="type-agent">Type of Agent:</label>
-                        <select id="type-agent" className='rounded p-2 mt-3 mb-5 w-100 credit-card-input formgray-text'>
-                            <option>Select</option>
-                            <option>Broker</option>
-                            <option>Agent</option>
-                            <option>Wholesaler</option>
-                            <option>Other</option>
-                        </select>
+                        <div className='rounded p-2 mt-3 mb-5 w-100 credit-card-input '>
+                            <select className='bg-white w-100 agent-select formgray-text'>
+                                <option>Select</option>
+                                <option>Broker</option>
+                                <option>Agent</option>
+                                <option>Wholesaler</option>
+                                <option>Other</option>
+                            </select>
+                        </div>
                     </div>
                     <div className="col-lg-10 mx-auto col-12  px-0 formgray-text text-center">
                         <h3 className='fs-33 fw-bold text-center mb-3 dark-blue-text'>
@@ -26,25 +35,28 @@ export default function AgentType() {
                     </div>
                     <div className="text-start col-md-6 px-md-4 px-0 formgray-text">
                         <label htmlFor="volume">Average Transaction volume (yearly):</label>
-                        <select id='volume' className='rounded p-2 mt-3 mb-5 w-100 credit-card-input formgray-text'>
-                            <option>Select</option>
-                            <option>0-1 Million</option>
-                            <option>1-5 Million</option>
-                            <option>5-25 Million</option>
-                            <option>+25 Million</option>
-                        </select>
+                        <div className='rounded p-2 mt-3 mb-5 w-100 credit-card-input '>
+                            <select className='bg-white w-100 agent-select formgray-text'>
+                                <option>Select</option>
+                                <option>0-1 Million</option>
+                                <option>1-5 Million</option>
+                                <option>5-25 Million</option>
+                                <option>+25 Million</option>
+                            </select>
+                        </div>
                     </div>
                     <div className="text-start col-md-6 px-md-4 px-0 formgray-text">
                         <label htmlFor="experience">Number of years of experience:</label>
-                        <select id="experience" className='rounded p-2 mt-3 mb-5 w-100 credit-card-input formgray-text'>
-                            <option>Select</option>
-                            <option>Less than 1 year</option>
-                            <option>1-3 Years</option>
-                            <option>3-5 Years</option>
-                            <option>5-10 Years</option>
-                            <option>10+ Years</option>
-
-                        </select>
+                        <div className='rounded p-2 mt-3 mb-5 w-100 credit-card-input '>
+                            <select className='bg-white w-100 agent-select formgray-text'>
+                                <option className="p-5">Select</option>
+                                <option className="p-5">Less than 1 year</option>
+                                <option className="p-5">1-3 Years</option>
+                                <option className="p-5">3-5 Years</option>
+                                <option className="p-5">5-10 Years</option>
+                                <option className="p-5">10+ Years</option>
+                            </select>
+                        </div>
                     </div>
                     <div className="text-start col-md-6 px-md-4 px-0 formgray-text">
                         <label htmlFor="company-name">Company Name (Optional):</label>
